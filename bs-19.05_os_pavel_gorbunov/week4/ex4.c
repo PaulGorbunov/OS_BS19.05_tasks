22d646aeccea1f8e7c03da87c3a59f0a4a2119ea
Paul_Gorbunov <pgrbnv@gmail.com>
2020-09-09 22:33:29 +0300

    week 4

--- bs-19.05_os_pavel_gorbunov/week4/ex4.c
+++ bs-19.05_os_pavel_gorbunov/week4/ex4.c
@@ -10,13 +10,13 @@ void execute(char c[100]) {
 int main() {
     char req[100];
     while (1) {
+        system("sleep 0.1");
         printf(":>>> ");
         fgets(req,100,stdin);
-        if (fork()) {
+        if (fork() == 0) {
             execute(req);
             return 0;
         }
-        
     }
     
     return 0;
