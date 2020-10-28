75c16ec8eb560c289ab18514eac72ceff5e5738b
Paul_Gorbunov <pgrbnv@gmail.com>
2020-10-28 23:56:21 +0300

    week11

--- bs-19.05_os_pavel_gorbunov/week11/ex4.c
+++ bs-19.05_os_pavel_gorbunov/week11/ex4.c
@@ -11,15 +11,15 @@ int main(){
     int f_orig = open("ex1.txt",O_RDWR);               //opening files
     int f_cop = open("ex1.memcpy.txt",O_RDWR);
     
-    struct stat statbuf;
+    struct stat buf;
     
-    fstat(f_orig,&statbuf);
-    ftruncate(f_cop,statbuf.st_size);
+    fstat(f_orig,&buf);
+    ftruncate(f_cop,buf.st_size);
     
-    char *orig = mmap(NULL,statbuf.st_size,PROT_READ|PROT_WRITE,MAP_SHARED,f_orig,0);   //mapping into the memory
-    char *extr = mmap(NULL,statbuf.st_size,PROT_READ|PROT_WRITE,MAP_SHARED,f_cop,0);
+    char *orig = mmap(NULL,buf.st_size,PROT_READ|PROT_WRITE,MAP_SHARED,f_orig,0);   //mapping into the memory
+    char *extr = mmap(NULL,buf.st_size,PROT_READ|PROT_WRITE,MAP_SHARED,f_cop,0);
     
-    memcpy(extr,orig,statbuf.st_size);
+    memcpy(extr,orig,buf.st_size);
 
     
     
